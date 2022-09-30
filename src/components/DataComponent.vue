f740a5d6f946da4fb3ebb557c015e692b23f12f5
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-09-30 10:41:08 +0700

    Use ref instead data()

---
+++ src/components/DataComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const firstName = ref("Taufan");
+firstName.value = "Evita";
+console.log(firstName);
+</script>
+
+<template>
+  <h2>Compositions - {{ firstName }}</h2>
+</template>
