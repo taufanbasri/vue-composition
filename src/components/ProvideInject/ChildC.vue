35eb82fa52b2e84eaeef56fee742466692a4d465
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-01 15:28:19 +0700

    Provide / Inject reactive

--- src/components/ProvideInject/ChildC.vue
+++ src/components/ProvideInject/ChildC.vue
@@ -1,10 +1,21 @@
 <script setup>
-import { inject } from "vue";
+import { inject, toRefs } from "vue";
 
 const username = inject("username", "Default Username");
+
+const count = inject("count", 0);
+const person = inject("person", {});
+const increment = inject("increment");
+
+const { firstName, lastName } = toRefs(person);
 </script>
 
 <template>
   <h2>Child C component</h2>
   <h3>Component C username - {{ username }}</h3>
+
+  <h3>Child Component C Count - {{ count }}</h3>
+  <h3>Child Component C Person - {{ firstName }} {{ lastName }}</h3>
+
+  <button @click="increment">Increment count from Child C</button>
 </template>
--- src/components/ProvideInject/ProvideInject.vue
+++ src/components/ProvideInject/ProvideInject.vue
@@ -1,14 +1,31 @@
 <script setup>
-import { provide, ref } from "vue";
+import { provide, reactive, ref, toRefs } from "vue";
 import ChildA from "./ChildA.vue";
 
 const name = ref("Taufan");
 
 provide("username", name);
+
+const count = ref(0);
+const state = reactive({
+  firstName: "Evita",
+  lastName: "Karlina",
+});
+
+const increment = () => count.value++;
+
+provide("count", count);
+provide("person", state);
+provide("increment", increment);
+
+const { firstName, lastName } = toRefs(state);
 </script>
 
 <template>
   <h3>Parent component {{ name }}</h3>
+  <h3>Parent component count {{ count }}</h3>
+  <h3>Parent component person {{ firstName }} {{ lastName }}</h3>
 
+  <button @click="increment">Increment Count</button>
   <ChildA />
 </template>
