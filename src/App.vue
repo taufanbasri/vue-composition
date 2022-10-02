642dd884a3b221c57690275650821b1f3491e4f1
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-02 13:52:03 +0700

    HTTP Post Request

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import PostList from "./components/http/PostList.vue";
+import CreatePost from "./components/http/CreatePost.vue";
 </script>
 
 <template>
+  <create-post />
   <post-list />
 </template>
---
+++ src/components/http/CreatePost.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { reactive } from "vue";
+import axios from "axios";
+
+const formData = reactive({
+  userId: "",
+  title: "",
+  body: "",
+});
+
+const createPost = () => {
+  axios
+    .post("https://jsonplaceholder.typicode.com/posts/", formData)
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+};
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="createPost">
+      <div>
+        <label for="userId">Post User Id</label>
+        <input type="text" id="userId" v-model="formData.userId" />
+      </div>
+      <div>
+        <label for="title">Post Title</label>
+        <input type="text" id="title" v-model="formData.title" />
+      </div>
+      <div>
+        <label for="body">Post Body</label>
+        <input type="text" id="body" v-model="formData.body" />
+      </div>
+      <button>Create Post</button>
+    </form>
+  </div>
+</template>
